a57fbae12cb9d0a06864a956c556f36639c448f2
olatubo <olatubo@dvcorporate.com>
2023-06-24 14:00:43 +0100

    adding oldertests to project

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -6,12 +6,10 @@ import (
 	"github.com/alexedwards/scs/v2"
 	"github.com/tsawler/bookings-app/internal/config"
 	"github.com/tsawler/bookings-app/internal/handlers"
-	"github.com/tsawler/bookings-app/internal/helpers"
 	"github.com/tsawler/bookings-app/internal/models"
 	"github.com/tsawler/bookings-app/internal/render"
 	"log"
 	"net/http"
-	"os"
 	"time"
 )
 
@@ -19,8 +17,6 @@ const portNumber = ":8080"
 
 var app config.AppConfig
 var session *scs.SessionManager
-var infoLog *log.Logger
-var errorLog *log.Logger
 
 // main is the main function
 func main() {
@@ -49,12 +45,6 @@ func run() error {
 	// change this to true when in production
 	app.InProduction = false
 
-	infoLog = log.New(os.Stdout, "INFO\t", log.Ldate|log.Ltime)
-	app.InfoLog = infoLog
-
-	errorLog = log.New(os.Stdout, "ERROR\t", log.Ldate|log.Ltime|log.Lshortfile)
-	app.ErrorLog = errorLog
-
 	// set up the session
 	session = scs.New()
 	session.Lifetime = 24 * time.Hour
@@ -75,8 +65,7 @@ func run() error {
 
 	repo := handlers.NewRepo(&app)
 	handlers.NewHandlers(repo)
-	render.NewTemplates(&app)
-	helpers.NewHelpers(&app)
 
+	render.NewTemplates(&app)
 	return nil
 }
